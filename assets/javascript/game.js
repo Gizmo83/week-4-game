2952f0f4026da071f0489d1108e8432f7687e619
Peter Hoang <peter.hoang83@gmail.com>
2018-02-19 16:30:48 -0800

    Updated Win/Lose notification

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
     var wins = 0;
     var losses = 0;
     var goal = "";
-    var userOption = []; //= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-    var userOptionSet = [];
+    var userOption = [];
+    var randomNumber;
     var userTotal = 0;
 
     function startGame (){
@@ -13,11 +13,13 @@ $(document).ready(function(){
         //console.log(goal);
         $("#goal").text(goal);
 
-        for (var i = 1; i <= 4; i++) {
-            userOption.push(Math.round(Math.random() * 11) +1);
-        
-            console.log(userOption)
-            $(".img-button-1").attr("data-buttonvalue",userOption[0]); //don't understand why userOption[i] doesn't work
+        while (userOption.length < 4) {
+            randomNumber = Math.floor(Math.random() * 11) +1;
+            if (userOption.indexOf(randomNumber) === -1) {
+                userOption.push(randomNumber);
+            }
+            //console.log(userOption)
+            $(".img-button-1").attr("data-buttonvalue",userOption[0]);
             $(".img-button-2").attr("data-buttonvalue",userOption[1]);
             $(".img-button-3").attr("data-buttonvalue",userOption[2]);
             $(".img-button-4").attr("data-buttonvalue",userOption[3]);
@@ -30,11 +32,13 @@ $(document).ready(function(){
         $(this).animate({opacity: '1'});
         var buttonValue = ($(this).attr("data-buttonvalue"));
         buttonValue = parseInt(buttonValue);
-        console.log(buttonValue);
+        //console.log(buttonValue);
         userTotal += buttonValue;
         $("#usertotal").html(userTotal);
         if (userTotal === goal) {
-            alert("You win!");
+            $("#result-1").animate({fontSize: "3em", opacity: '1'}, "slow");
+            $("#result-1").animate({fontSize: "1em", opacity: '0'}, "slow");
+            //alert("You win!");
             wins++;
             $(".wins").html(wins);
             userTotal = 0;
@@ -43,7 +47,9 @@ $(document).ready(function(){
             startGame();
         }
         else if (userTotal > goal) {
-            alert("You lost");
+            $("#result-2").animate({fontSize: "3em", opacity: '1'}, "slow");
+            $("#result-2").animate({fontSize: "1em", opacity: '0'}, "slow");
+            //alert("You lost");
             losses++;
             $(".losses").html(losses);
             userTotal = 0;
